0d0a0846cb278954ba2779a38baedfe14eb6ffd3
Ana Ganfornina <anies.llamani@gmail.com>
2024-12-16 19:12:48 +0100

    wip:to_print_result

--- calc_romanos.py
+++ calc_romanos.py
@@ -1,23 +1,37 @@
 
 from func_calc import ask_operation,calculation,imput_to_int,is_valid,ask_input
-from fromanos import RomanNumberError
+from enum import Enum
+
+class Opeation(Enum):
+    SUMAR = "+"
+    RESTAR = "-"
+    MULTIPLICAR = "*"
+    DIVIDIR = "/"
 
 
+def to_print_result(first:int,operation:int,second:int):
+
+    result = calculation(first,second,operation)
+
+    print(f"{first} {operation} {second} = {result}")
 
 
 def calculatum():
+
+    result = None
    
-    is_exit = "S" #preguntate si estoy hay que refacotrizarlo por un continue
-    while is_exit == "S":
+    other_operation = "S" 
+    while other_operation == "S":
         
         operador = int(ask_operation())
         first_number = ask_input("Primer número:")
 
         second_number = ask_input("Segundo número:")
-        
-        print(calculation(first_number,second_number,operador))
 
-        is_exit = input("Otra S/N: ").upper()
+        to_print_result(first_number,operador,second_number)
+        
+        
+        other_operation = input("Otra S/N: ").upper()
 
 
 
